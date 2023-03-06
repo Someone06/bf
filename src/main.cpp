7a14e8179b636858104a81e15f6df542271d8b32
Michael Pusl <pusl03@ads.uni-passau.de>
2023-03-06 13:24:46 +0100

    Added: Main: Use Lexer and Sourcerator.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <vector>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+#include "Lexer.h"
+#include "Sourcerator.h"
+
+int main(int argc, char* argv[]) {
+    if(argc < 2) {
+        std::cerr << "Args: Input file";
+        return 1;
+    }
+
+    auto file = std::unique_ptr<std::istream>(new std::ifstream{argv[1]});
+    Lexer lexer{std::move(file)};
+    std::function<std::optional<Token>()> f {[l = std::make_shared<Lexer>(std::move(lexer))]() mutable {return l->next();}};
+    auto iter = Sourcerator<Token>{std::move(f)};
     return 0;
 }
