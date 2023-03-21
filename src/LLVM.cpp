49d7d1cad2c09c2fdad4bddb7f69c89c64bb6528
Michael Pusl <pusl03@ads.uni-passau.de>
2023-03-21 12:26:56 +0100

    Refactor: Cleanup.

--- src/LLVM.cpp
+++ src/LLVM.cpp
@@ -3,8 +3,65 @@
 #include <llvm/IR/AssemblyAnnotationWriter.h>
 #include <llvm/IR/Verifier.h>
 #include <llvm/Support/raw_os_ostream.h>
+#include <llvm/IR/Function.h>
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/Value.h>
 
 #include "LLVM.h"
+#include "AstVisitors.h"
+
+class LLVM final : private ASTWalker {
+public:
+    LLVM(AST& ast, std::unordered_map<const While*, const Node*> next, std::ostream &out)
+        : ASTWalker{ast},
+          nextInstruction {std::move(next)},
+          ctxt{llvm::LLVMContext()},
+          mod{llvm::Module{"main", ctxt}},
+          bd{llvm::IRBuilder(ctxt, llvm::ConstantFolder())},
+          o{out} {}
+
+    llvm::Module& generate_ir();
+
+private:
+    llvm::Function& createMainFunction();
+    llvm::BasicBlock& createInitialBasicBlock(llvm::Function& mainFun);
+    std::pair<llvm::BasicBlock*, bool> createNextBlock(const While &aWhile);
+
+    llvm::Value &createMem();
+    llvm::Value &createMemPtr();
+
+    llvm::Value& createGEP();
+    llvm::Value& read();
+    void write(llvm::Value& val);
+    void inc(uint64_t amount);
+    void dec(uint64_t amount);
+
+
+    void visit(const Left &left) override;
+    void visit(const Right &right) override;
+    void visit(const Inc &inc) override;
+    void visit(const Dec &dec) override;
+    void visit(const In &in) override;
+    void visit(const Out &out) override;
+    void visit(const While &aWhile) override;
+
+    uint64_t memSz {30'000};
+
+    llvm::LLVMContext ctxt;
+    llvm::Module mod;
+    llvm::IRBuilder<> bd;
+
+    llvm::Value* mem {nullptr};
+    llvm::Value* ptr {nullptr};
+
+    llvm::Function * mainFn {nullptr};
+
+    std::unordered_map<const While*, const Node*> nextInstruction;
+    std::unordered_map<const Node*, llvm::BasicBlock*> blockForNode {};
+
+    std::ostream& o;
+};
 
 llvm::Module& LLVM::generate_ir() {
     mainFn = &createMainFunction();
@@ -34,6 +91,7 @@ llvm::Function& LLVM::createMainFunction() {
                 mod
             );
 }
+
 llvm::BasicBlock& LLVM::createInitialBasicBlock(llvm::Function& mainFun){
     const auto block {llvm::BasicBlock::Create(ctxt, "entry", &mainFun)};
     bd.SetInsertPoint(block);
@@ -148,3 +206,8 @@ void LLVM::dec(uint64_t amount) {
    bd.CreateStore(inc, ptr, "ptr_store");
 }
 
+llvm::Module &generate_ir(AST &ast,
+                          std::unordered_map<const While *, const Node *> next,
+                          std::ostream &out) {
+   return LLVM(ast, std::move(next), out).generate_ir();
+}
--- src/LLVM.h
+++ src/LLVM.h
@@ -2,67 +2,12 @@
 #define BF_LLVM_H
 
 #include <ostream>
+#include <unordered_map>
 
-#include <llvm/IR/Function.h>
-#include <llvm/IR/IRBuilder.h>
-#include <llvm/IR/LLVMContext.h>
 #include <llvm/IR/Module.h>
-#include <llvm/IR/Value.h>
 
 #include "AST.h"
-#include "AstVisitors.h"
 
-class LLVM final : private ASTWalker {
-public:
-    LLVM(AST& ast, std::unordered_map<const While*, const Node*> next, std::ostream &out)
-        : ASTWalker{ast},
-          nextInstruction {std::move(next)},
-          ctxt{llvm::LLVMContext()},
-          mod{llvm::Module{"main", ctxt}},
-          bd{llvm::IRBuilder(ctxt, llvm::ConstantFolder())},
-          o{out} {}
-
-    llvm::Module& generate_ir();
-
-private:
-    llvm::Function& createMainFunction();
-    llvm::BasicBlock& createInitialBasicBlock(llvm::Function& mainFun);
-    std::pair<llvm::BasicBlock*, bool> createNextBlock(const While &aWhile);
-
-    llvm::Value &createMem();
-    llvm::Value &createMemPtr();
-
-    llvm::Value& createGEP();
-    llvm::Value& read();
-    void write(llvm::Value& val);
-    void inc(uint64_t amount);
-    void dec(uint64_t amount);
-
-
-    void visit(const Left &left) override;
-    void visit(const Right &right) override;
-    void visit(const Inc &inc) override;
-    void visit(const Dec &dec) override;
-    void visit(const In &in) override;
-    void visit(const Out &out) override;
-    void visit(const While &aWhile) override;
-
-    uint64_t memSz {30'000};
-
-    llvm::LLVMContext ctxt;
-    llvm::Module mod;
-    llvm::IRBuilder<> bd;
-
-    llvm::Value* mem {nullptr};
-    llvm::Value* ptr {nullptr};
-
-    llvm::Function * mainFn {nullptr};
-
-    std::unordered_map<const While*, const Node*> nextInstruction;
-    std::unordered_map<const Node*, llvm::BasicBlock*> blockForNode {};
-
-    std::ostream& o;
-};
-
+llvm::Module& generate_ir(AST& ast, std::unordered_map<const While*, const Node*> next, std::ostream &out);
 
 #endif
